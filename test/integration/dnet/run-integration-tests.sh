157a7be0225578161b12f9cca96b18c9ba07c742
Hui Kang <kangh@us.ibm.com>
2017-02-22 11:37:43 -0500

    Move function

--- test/integration/dnet/run-integration-tests.sh
+++ test/integration/dnet/run-integration-tests.sh
@@ -151,6 +151,39 @@ function run_simple_consul_tests() {
     unset cmap[dnet-1-simple]
 }
 
+function test_multi_service_attach() {
+    echo $(docker ps)
+    dnet_cmd $(inst_id2port 2) network create -d test multihost
+    dnet_cmd $(inst_id2port 3) service publish svc.multihost
+    for i in `seq 1 3`;
+    do
+	    dnet_cmd $(inst_id2port $i) container create container_${i}
+	    dnet_cmd $(inst_id2port $i) service attach container_${i} svc.multihost
+        dnet_cmd $(inst_id2port $i) service ls
+	    #run dnet_cmd $(inst_id2port $i) service ls
+	    #[ "$status" -eq 0 ]
+	    #echo ${output}
+	    #echo ${lines[1]}
+	    #container=$(echo ${lines[1]} | cut -d" " -f4)
+	    #[ "$container" = "container_$i" ]
+	    for j in `seq 1 3`;
+	    do
+	        if [ "$j" = "$i" ]; then
+		        continue
+	        fi
+	        dnet_cmd $(inst_id2port $j) container create container_${j}
+	        dnet_cmd $(inst_id2port $j) service attach container_${j} svc.multihost
+	        #echo ${output}
+	        #[ "$status" -ne 0 ]
+	        dnet_cmd $(inst_id2port $j) container rm container_${j}
+	    done
+	    dnet_cmd $(inst_id2port $i) service detach container_${i} svc.multihost
+	    dnet_cmd $(inst_id2port $i) container rm container_${i}
+    done
+    dnet_cmd $(inst_id2port 1) service unpublish svc.multihost
+    dnet_cmd $(inst_id2port 3) network rm multihost
+}
+
 function run_multi_consul_tests() {
     # Test multi node configuration with a global scope test driver backed by consul
 
@@ -167,6 +200,7 @@ function run_multi_consul_tests() {
     docker info
     dnet_cmd $(inst_id2port 2) service ls
     ./integration-tmp/bin/bats ./test/integration/dnet/multi.bats
+    test_multi_service_attach
 
     ## Teardown
     stop_dnet 1 multi_consul 1>>${INTEGRATION_ROOT}/test.log 2>&1
